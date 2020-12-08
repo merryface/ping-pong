053c54ecba08b1fd98d720eb5c713664760d6e2d
Tarik <tarikmerrylees@gmail.com>
2020-12-08 16:10:28 +0000

    keep record of games

--- src/data/recordGame.js
+++ src/data/recordGame.js
@@ -1,30 +1,25 @@
 //Store game result
 let result = (state) => {
-
-    if (state.winner !== 0) {
-
-        return (
-            {
-                player_1: {
+    return (
+        {
+            player_1: {
                 score: state.player1,
                 won: state.winner === 1
-                },
-        
-                player_2: {
+            },
+    
+            player_2: {
                 score: state.player2,
                 won: state.winner === 2
-                }
             }
-        )
-    }
-
+        }
+    )
 }
 
 // push new result into record
 let recordGame = (state) => (
     {
         ...state,
-        record: [...state.record, result]
+        record: state.winner !== 0 ? [...state.record, result(state)] : state.record
     }
 )
 
--- src/data/reducer.js
+++ src/data/reducer.js
@@ -3,17 +3,17 @@ import declareWinner from './winner';
 import serveChange from './server';
 import player1 from './player1Score';
 import player2 from './player2Score';
-// import recordGame from './recordGame';
+import recordGame from './recordGame';
 
 
 //reducer
 let reducer = (state, action) => {
     switch (action.type) {
         case "INCREMENTPLAYER1":
-            return declareWinner(serveChange(player1(state)));
+            return recordGame(declareWinner(serveChange(player1(state))));
 
         case "INCREMENTPLAYER2":
-            return declareWinner(serveChange(player2(state)));
+            return recordGame(declareWinner(serveChange(player2(state))));
 
         case "RESET": return initialState;
 
